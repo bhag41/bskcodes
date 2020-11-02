284d0868ecdd243aa6889278b23b17ef5579f77f
Bhagyashree <bhag41@gmail.com>
2020-11-01 18:07:46 -0800

    first commit

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -93,7 +93,7 @@ export const pageQuery = graphql`
       id
       html
       frontmatter {
-        date(formatString: "MMMM DD, YYYY")
+        date(formatString: "MM DD, YYYY")
         title
         description
         tags
