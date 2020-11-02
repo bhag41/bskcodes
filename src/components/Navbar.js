512cf05c1dfe9ed3a5716b644f1bf064d03893c7
Bhagyashree <bhag41@gmail.com>
2020-11-01 18:32:02 -0800

    logo size updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -42,7 +42,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="bskcodes" style={{ width: '88px' }} />
+              <img src={logo} alt="bskcodes" style={{ width: '300px' }} />
             </Link>
             {/* Hamburger menu */}
             <div
@@ -62,9 +62,6 @@ const Navbar = class extends React.Component {
             <div className="navbar-start has-text-centered">
               <Link className="navbar-item" to="/about">
                 About
-              </Link>
-              <Link className="navbar-item" to="/products">
-                Products
               </Link>
               <Link className="navbar-item" to="/blog">
                 Blog
